4027588e5e8e6b3aa155d63e835c67c07cd273d9
mariocg5 <mariocuevas918@gmail.com>
2024-10-18 10:55:12 +0200

    18/10/23

--- Tema1/src/ficheros/Ejercicio12.java
+++ Tema1/src/ficheros/Ejercicio12.java
@@ -19,6 +19,8 @@ import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.w3c.dom.Text;
 
 import ficheros.ejercicio6.Ejercicio6;
@@ -30,7 +32,8 @@ public class Ejercicio12 {
 
 	public static void main(String[] args) {
 		leerObjetos();
-
+		LeerXML();
+		
 	}
 	
 	public static void leerObjetos() { // void o persona, preguntar
@@ -48,30 +51,13 @@ public class Ejercicio12 {
 					persona = (Persona) oIS.readObject();
 					Element elementoPersona = doc.createElement("Persona");
 					
-					Element elementoNombre = doc.createElement("nombre");
-					Text nombreText = doc.createTextNode(persona.getNombre().toString());
-					elementoNombre.appendChild(nombreText);
+					CreaElemento("nombre",persona.getNombre().toString(),elementoPersona, doc );
+					CreaElemento("apellido1",persona.getApellido1().toString(),elementoPersona, doc );
+					CreaElemento("apellido2",persona.getApellido2().toString(),elementoPersona, doc );
+					CreaElemento("nacimiento",persona.getNacimiento().toString(),elementoPersona, doc );
 					
-					Element elementoApellido1 = doc.createElement("apellido");
-					Text apellidoText=doc.createTextNode(persona.getApellido1().toString());
-					elementoApellido1.appendChild(apellidoText);
 					
-					Element elementoApellido2=doc.createElement("apellido2");
-					Text apellido2Text = doc.createTextNode(persona.getApellido2().toString());
-					elementoApellido2.appendChild(apellido2Text);
-					
-					Element elementoNacimiento = doc.createElement("nacimiento");
-					Text nacimientoText = doc.createTextNode(persona.getNacimiento().toString());
-					elementoNacimiento.appendChild(nacimientoText);
-					
-					elementoPersona.appendChild(elementoNombre);
-					elementoPersona.appendChild(elementoApellido1);
-					elementoPersona.appendChild(elementoApellido2);
-					elementoPersona.appendChild(elementoNacimiento);
-					
-					raiz.appendChild(elementoPersona);
-					
-				
+					raiz.appendChild(elementoPersona);			
 				}
 			} catch (ClassNotFoundException e) {
 				System.out.println("Error de lectura en el fichero");
@@ -85,7 +71,7 @@ public class Ejercicio12 {
 			TransformerFactory tF= TransformerFactory.newInstance();
 			Transformer t = tF.newTransformer();
 			t.setOutputProperty(OutputKeys.INDENT, "yes");
-			t.transform(new DOMSource(doc), new StreamResult(System.out));
+			//t.transform(new DOMSource(doc), new StreamResult(System.out));
 			t.transform(new DOMSource(doc), new StreamResult(new File(Ejercicio8.RUTA+DOCTRABAJO)));
 
 		} catch (IOException e) {
@@ -99,6 +85,53 @@ public class Ejercicio12 {
 			e.printStackTrace();
 		}
 
+	}
+
+	private static void CreaElemento(String etiqueta, String valor, Element padre, Document doc) {
+		Element elem = doc.createElement(etiqueta);
+		Text texto = doc.createTextNode(valor);
+		padre.appendChild(elem);
+		elem.appendChild(texto);		
+	}
+	
+	public static void LeerXML() {
+		   try {
+		       DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		       DocumentBuilder builder = factory.newDocumentBuilder();
+		       
+		       //Cargamos en memoria el doc XML        
+		       Document doc = builder.parse(new File(Ejercicio8.RUTA+DOCTRABAJO));
+		       
+		       LeeNodo(doc.getDocumentElement());
+		       
+		   } catch (Exception e) {e.printStackTrace();}
+	}
+		       
+	public static void LeeNodo(Node nodo) {
+		
+		   if(nodo.getNodeType()==Node.ELEMENT_NODE)    {
+			   Element element = (Element) nodo;
+			   System.out.print("<"+element.getNodeName()+">");
+			   
+			   NodeList nodosHijos= element.getChildNodes();
+			   if(nodosHijos.item(0).getNodeType()==Node.ELEMENT_NODE) {
+				   System.out.println("\t");
+			   }
+			   for (int i = 0; i < nodosHijos.getLength(); i++) {
+				   LeeNodo(nodosHijos.item(i));
+			}
+			   System.out.println("</"+element.getNodeName()+">");
+			   
+		   } else if(nodo.getNodeType()==Node.TEXT_NODE) {
+			   
+			   String texto= nodo.getNodeValue();
+			   if(!texto.isEmpty()) {
+				   System.out.print(texto);
+			   }
+		   }
+		   
+		  
+	
 	}
 	
 	
